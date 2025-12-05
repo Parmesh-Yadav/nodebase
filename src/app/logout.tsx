90a05cf6803da60655edde4789b780d8c54299f0
Parmesh Yadav <parmesh20319@iiitd.ac.in>
2025-12-06 03:37:03 +0530

    04: authentication

---
+++ src/app/logout.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+
+export const LogOutButton = () => {
+    const router = useRouter();
+    return (
+        <Button onClick={() => authClient.signOut({
+            fetchOptions: {
+                onSuccess: () => {
+                    router.push("/");
+                }
+            }
+        })}>
+            Log Out
+        </Button>
+    );
+}
