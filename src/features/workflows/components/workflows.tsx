9772dbcd9371d1438431c48f50665eddd0546626
Parmesh Yadav <parmesh20319@iiitd.ac.in>
2025-12-18 22:20:57 +0530

    feat: integrate nuqs for workflows management with search and pagination features

--- src/features/workflows/components/workflows.tsx
+++ src/features/workflows/components/workflows.tsx
@@ -1,9 +1,27 @@
 "use client"
 
-import { EntityContainer, EntityHeader } from "@/components/entity-components";
-import { useCreateWorkflow, useSuspenseWorkflows } from "../hooks/user-workflows";
-import { useUpgradeModal } from "../hooks/use-upgrade-modal";
+import { EntityContainer, EntityHeader, EntityPagination, EntitySearch } from "@/components/entity-components";
+import { useCreateWorkflow, useSuspenseWorkflows } from "../hooks/use-workflows";
+import { useUpgradeModal } from "../../../hooks/use-upgrade-modal";
 import { useRouter } from "next/navigation";
+import { useWorkflowsParams } from "../hooks/use-workflows-params";
+import { useEntitySeach } from "@/hooks/use-entity-search";
+
+export const WorkflowsSearch = () => {
+    const [params, setParams] = useWorkflowsParams();
+    const { searchValue, onSearchChange } = useEntitySeach({
+        params,
+        setParams
+    })
+
+    return (
+        <EntitySearch
+            value={searchValue}
+            onChange={onSearchChange}
+            placeholder="Search workflows"
+        />
+    );
+}
 
 export const WorkflowsList = () => {
     const workflows = useSuspenseWorkflows();
@@ -46,13 +64,27 @@ export const WorkflowsHeader = ({ disabled }: { disabled?: boolean }) => {
     );
 };
 
+export const WorkflowsPagination = () => {
+    const workflows = useSuspenseWorkflows();
+    const [params, setParams] = useWorkflowsParams();
+
+    return (
+        <EntityPagination
+            disabled={workflows.isFetching}
+            totalPages={workflows.data.totalPages}
+            page={workflows.data.page}
+            onPageChange={(page) => setParams({ ...params, page })}
+        />
+    );
+}
+
 export const WorkflowsContainer = ({ children }: { children: React.ReactNode }) => {
 
     return (
         <EntityContainer
             header={<WorkflowsHeader />}
-            search={<></>}
-            pagination={<></>}
+            search={<WorkflowsSearch />}
+            pagination={<WorkflowsPagination />}
         >
             {children}
         </EntityContainer>
