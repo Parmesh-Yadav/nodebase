9e2508bfeb1be4e9b7722f453fc28a9d6ef0cfb0
Parmesh Yadav <parmesh20319@iiitd.ac.in>
2025-12-22 21:32:22 +0530

    feat: enhance workflows editor with loading/error states and CRUD functionality

--- src/features/workflows/server/prefetch.ts
+++ src/features/workflows/server/prefetch.ts
@@ -4,7 +4,11 @@ import { prefetch, trpc } from "@/trpc/server";
 type Input = inferInput<typeof trpc.workflows.getMany>;
 
 // prefetch all workflows
-
 export const prefetchWorkflows = (params: Input) => {
   return prefetch(trpc.workflows.getMany.queryOptions(params));
+};
+
+// prefetch single workflow by id
+export const prefetchWorkflow = (id: string) => {
+  return prefetch(trpc.workflows.getOne.queryOptions({ id }));
 };
